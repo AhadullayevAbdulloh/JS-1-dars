af7a566d52a726bc8438e069084b3ea69e884f7d
AhadullayevAbdulloh <147840304+AhadullayevAbdulloh@users.noreply.github.com>
2023-10-17 20:51:51 +0500

    JS 1-dars
    27-Guruh

---
+++ JS 1-dars/js/main.js
@@ -0,0 +1,38 @@
+
+// console.log("Men console oynasida ko'rinaman!");
+// document.write("Men body oynasida ko'rinaman!");
+// alert("Saytimizga hush kelibsiz");
+// console.log(5-10*5);
+
+// let x = 2;
+// let y = 5;
+// let z = 10;
+// x = 7;
+// let x = 8, y = 6, z = 9;
+// document.write(x+y+z);
+
+// document.write("Assalomu Alaykum
+// let x = 5;
+// let y = "JavaScript"
+// document.write(typeof y);
+
+let x = 5;
+// x+=1;
+// document.write(x);
+
+// let c = 5;
+// c-=3;
+// document.write(c);
+
+// let v = 8;
+// v*=4;
+// document.write(v);
+
+// let qoldiq = x%4;
+// let daraja = x**2
+--x;
+--x;
+--x;
+--x;
+-- x;
+document.write(x);
